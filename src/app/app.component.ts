5df222c8f11d1abf4a6ab9780f2515ef664d8eaa
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-04-14 12:40:11 +0530

    ui update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,59 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {}
+export class AppComponent {
+  Questions = [
+    {
+      name: 'Question 1',
+      path: 'question-1',
+    },
+    {
+      name: 'Question 2',
+      path: 'question-2',
+    },
+    {
+      name: 'Question 3',
+      path: 'question-3',
+    },
+    {
+      name: 'Question 4',
+      path: 'question-4',
+    },
+    {
+      name: 'Question 5',
+      path: 'question-5',
+    },
+    {
+      name: 'Question 6',
+      path: 'question-6',
+    },
+    {
+      name: 'Question 7',
+      path: 'question-7',
+    },
+    {
+      name: 'Question 8',
+      path: 'question-8',
+    },
+    {
+      name: 'Question 9',
+      path: 'question-9',
+    },
+    {
+      name: 'Question 10',
+      path: 'question-10',
+    },
+    {
+      name: 'Question 11',
+      path: 'question-11',
+    },
+    {
+      name: 'Question 12',
+      path: 'question-12',
+    },
+    {
+      name: 'Question 13',
+      path: 'question-13',
+    },
+  ];
+}
